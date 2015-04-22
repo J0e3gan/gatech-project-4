a194b0922d0941f254ecee2d08b3cfdddb46c820
Pawel Drozdz <Pawel.Drozdz@ge.com>
2015-04-21 20:52:22 -0700

    Student update convenience method endpoint, trigger schedule engine

---
+++ course-optimizer/src/main/java/gatech/course/optimizer/dto/StudentUpdateRequest.java
@@ -0,0 +1,30 @@
+package gatech.course.optimizer.dto;
+
+import java.util.List;
+
+/**
+ * Created by 204069126 on 4/21/15.
+ */
+public class StudentUpdateRequest {
+
+    String studentId;
+    List<DesiredCourseDTO> desiredCourses;
+
+    public StudentUpdateRequest() {}
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public List<DesiredCourseDTO> getDesiredCourses() {
+        return desiredCourses;
+    }
+
+    public void setDesiredCourses(List<DesiredCourseDTO> desiredCourses) {
+        this.desiredCourses = desiredCourses;
+    }
+}
--- course-optimizer/src/main/java/gatech/course/optimizer/service/StudentService.java
+++ course-optimizer/src/main/java/gatech/course/optimizer/service/StudentService.java
@@ -1,19 +1,17 @@
 package gatech.course.optimizer.service;
 
+import gatech.course.optimizer.dto.DesiredCourseDTO;
 import gatech.course.optimizer.dto.StudentDTO;
-import gatech.course.optimizer.dto.TakenCourseDTO;
-import gatech.course.optimizer.model.CourseOffering;
+import gatech.course.optimizer.dto.StudentUpdateRequest;
+import gatech.course.optimizer.model.Course;
+import gatech.course.optimizer.model.DesiredCourse;
 import gatech.course.optimizer.model.Student;
-import gatech.course.optimizer.repo.CourseOfferingRepo;
-import gatech.course.optimizer.repo.StudentDetailsRepo;
-import gatech.course.optimizer.repo.StudentRecordRepo;
-import gatech.course.optimizer.repo.StudentRepo;
+import gatech.course.optimizer.repo.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,17 +19,27 @@ import java.util.List;
  */
 @RestController
 public class StudentService {
+
     @Autowired
     private StudentRepo studentRepo;
 
     @Autowired
-    private CourseOfferingRepo courseOfferingRepo;
+    private CourseRepo courseRepo;
 
     @Autowired
-    private StudentRecordRepo studentRecordRepo;
+    private DesiredCourseRepo desiredCourseRepo;
 
     @Autowired
     public StudentDetailsRepo studentDetailsRepo;
+
+    @Autowired
+    private ExecutionTrigger executionTrigger;
+
+
+    // TODO: create enum
+    public int NEW = 1;
+    public int UPDATED = 2;
+    public int NO_CHANGE = 3;
 
     public static Logger logger = LoggerFactory.getLogger(StudentService.class);
 
@@ -60,15 +68,65 @@ public class StudentService {
         return studentRepo.save(student);
     }
 
-    /*
-    @RequestMapping(value = "/student", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/student/update", method = RequestMethod.POST)
     public
     @ResponseBody
-    StudentDTO updateStudent(@RequestBody StudentDTO studentDTO) {
-        logger.info("Updating student");
-        studentRepo.save(studentDTO.toStudent());
-        studentDTO.ge
-        return null;
+    StudentDTO updateStudent(@RequestBody StudentUpdateRequest studentUpdateRequest) {
+        logger.info("Updating student " + studentUpdateRequest.getStudentId());
+        Student student = studentRepo.findByStudentId(studentUpdateRequest.getStudentId());
+        if (student != null) {
+            List<DesiredCourse> desiredCourses = student.getDesiredCourses();
+            for (DesiredCourseDTO desiredCourseDTO : studentUpdateRequest.getDesiredCourses()) {
+                int courseChangeStatus = isNewOrUpdated(desiredCourses,desiredCourseDTO);
+
+                if(courseChangeStatus == NEW){
+                    logger.info("New desired course added : "+desiredCourseDTO.getCourseId() + " - " +desiredCourseDTO.getPriority());
+                    Course course = courseRepo.findOne(desiredCourseDTO.getCourseId());
+                    DesiredCourse newDesiredCourse = new DesiredCourse(student.getId(),course,desiredCourseDTO.getPriority());
+                    newDesiredCourse = desiredCourseRepo.save(newDesiredCourse);
+                    desiredCourses.add(newDesiredCourse);
+                } else if(courseChangeStatus == UPDATED) {
+                    logger.info("Priority updated for "+desiredCourseDTO.getCourseId() + " - " +desiredCourseDTO.getPriority());
+                    for(DesiredCourse desiredCourse : desiredCourses){
+                        if(desiredCourse.getCourse().getId().longValue() == desiredCourseDTO.getCourseId().longValue()){
+                            desiredCourse.setPriority(desiredCourseDTO.getPriority());
+                            desiredCourseRepo.save(desiredCourse);
+                        }
+                    }
+                } else {
+                    logger.info("No change to desired course "+desiredCourseDTO.getCourseId() + " - " +desiredCourseDTO.getPriority());
+                }
+            }
+            student.setDesiredCourses(desiredCourses);
+            studentRepo.save(student);
+
+
+            // Trigger engine orchestration since student updated its desired courses
+            executionTrigger.createScheduleSolution();
+
+            // Return updated student
+            return studentDetailsRepo.getStudentDetails(student.getStudentId());
+        }
+
+        logger.error("Student not found for id " +studentUpdateRequest.getStudentId());
+        throw new RuntimeException("Student not found");
     }
-    */
+
+
+    private int isNewOrUpdated(List<DesiredCourse> desiredCourses, DesiredCourseDTO desiredCourseDTO) {
+        for (DesiredCourse desiredCourse : desiredCourses) {
+            // Course was already on the list
+            if (desiredCourse.getCourse().getId() == desiredCourseDTO.getCourseId()) {
+                // Course was already on the list and it has the same priority
+                if (desiredCourse.getPriority() == desiredCourseDTO.getPriority().intValue()) {
+                    return NO_CHANGE; // NO CHANGE
+                } else {
+                    return UPDATED; // UPDATED PRIORITY
+                }
+            }
+        }
+        return NEW; // NEW
+    }
+
 }
